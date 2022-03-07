1357356e6431a5507dd293709a97bc25882726a4
Jatin <38358079+jxtin@users.noreply.github.com>
2022-03-07 11:56:55 +0530

    auto-generated csv name

--- run.py
+++ run.py
@@ -12,7 +12,10 @@ print(list_parameters)
 for param in list_parameters:
     print("param: ")
     print(param)
+    param["CSV"] = param["query"].replace(" ", "_").replace(",", "") + ".csv"
+    param["CSV"] = param["CSV"].lower()
     cmd_str = f"scrapy crawl airbnb -a query=\"{param['query']}\", -o {param['CSV']}"
+    print(cmd_str)
     # run scrapy
     os.system(cmd_str)
     print("\n")
